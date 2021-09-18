ae0444df654cd4d2a182539d6c1a4ba4facd644d
Drizzle042 <chukwujiobicanon@gmail.com>
2021-09-18 09:43:53 +0100

    update to database, models and admin site

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Email, Profile 
+from .models import Email, Profile, Profile_event_photo 
 
+class Inline_stack(admin.StackedInline):
+    model = Profile_event_photo
 # Register your models here.
-
 admin.site.register(Email),
-admin.site.register(Profile),
+
+@admin.register(Profile)
+class Inline(admin.ModelAdmin):
+    inlines = [Inline_stack]
+
+    class Meta:
+        model = Profile
+
+@admin.register(Profile_event_photo)
+class Inline_stack(admin.ModelAdmin):
+    pass
--- home/models.py
+++ home/models.py
@@ -11,8 +11,8 @@ class Email(models.Model):
 
 # The profiles database Modelform to map to database
 class Profile(models.Model):
-	profile_cover_photo = models.ImageField(default = 'profile-photo.png', blank = True)
-	profile_photo = models.ImageField(default = 'profile-photo.png', blank = True)
+	profile_cover_photo = models.ImageField(default = 'profile-photo.png', blank = True, upload_to = 'media/profile/profile_cover_photos/')
+	profile_photo = models.ImageField(default = 'profile-photo.png', blank = True, upload_to = 'media/profile/profile_photos/')
 	first_name = models.CharField(max_length = 14)
 	last_name = models.CharField(max_length = 14)
 	slug = models.SlugField()
@@ -20,7 +20,6 @@ class Profile(models.Model):
 	followers = models.IntegerField(default = 1, blank = True)
 	events = models.IntegerField(default = 0, blank = True)
 	donations = models.IntegerField(default = 0, blank = True)
-	event_photos = models.ImageField(blank = True)
 	address_field = models.CharField(max_length = 40, blank = True)
 	mobile_contact = models.PositiveBigIntegerField(blank = True)
 	email = models.EmailField(blank = True)
@@ -28,3 +27,14 @@ class Profile(models.Model):
 	twitter_contact = models.CharField(max_length = 20, blank = True)
 	about = models.TextField()
 	date = models.DateField()
+
+	def __str__(self):
+		return self.slug
+	
+# Event_photos database Modelform to map to database. This inherits from Profile and attaches here 
+class Profile_event_photo(models.Model):
+	profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
+	event_photos = models.ImageField(blank = True, upload_to = 'media/profile/events_images/')
+
+	def __str__(self):
+		return self.profile.slug
