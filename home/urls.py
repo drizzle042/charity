4d5a73eaf0cd438738935202ff2bc59f0a5eb5bf
Drizzle042 <chukwujiobicanon@gmail.com>
2021-09-27 01:11:00 +0100

    new database fields for events page and rendered on events page

--- home/urls.py
+++ home/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     path('events/', views.events_page, name='events'),
     path('story/<slug:slug>/', views.story_detail, name='detail'),
+    path('topstory/<slug:slug>/', views.topstories, name='topstory'),
+    path('ad/<slug:slug>/', views.advert, name='advert'),
     path('about/', views.about_page, name='about'),
     path('profiles/', views.profile_feed, name='profiles'),
-    path('donate/', views.donation_page, name='donate'),
     path('profile/<slug:slug>/', views.profile_page, name='profile'),
+    path('donate/', views.donation_page, name='donate'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .forms import Form
-from .models import Profile, Profile_event_photo, Headline
+from .models import Advert, Advert_image, Profile, Profile_event_photo, Headline, Topstory, Topstory_image
 from django.contrib import messages 
 from django.core.paginator import Paginator
 
@@ -24,10 +24,18 @@ def home_page(request, *args, **kwargs):
 
 def events_page(request, *args, **kwargs):
 	events_objects = Headline.objects.all().order_by('-date')
-	paginate = Paginator(object_list=events_objects, per_page=4, orphans=2, allow_empty_first_page=True)
+	Advert_objects = Advert.objects.all().order_by('-date')
+	Topstory_objects = Topstory.objects.all().order_by('-date')
+
+	paginate_events = Paginator(object_list=events_objects, per_page=4, orphans=2, allow_empty_first_page=True)
+	paginate_advert = Paginator(object_list=Advert_objects, per_page=1, allow_empty_first_page=True)
+	paginate_topstory = Paginator(object_list=Topstory_objects, per_page=1, allow_empty_first_page=True)
 
 	page=request.GET.get('page')
-	events=paginate.get_page(page)
+	events=paginate_events.get_page(page)
+	advert=paginate_advert.get_page(page)
+	topstory=paginate_topstory.get_page(page)
+
 	form = Form(auto_id=False)
 	if request.method == 'POST':
 		form = Form(request.POST)
@@ -37,27 +45,14 @@ def events_page(request, *args, **kwargs):
 			messages.success(request, "Your email was received successfully!", fail_silently = True)
 	else:
 		form = Form()
+
 	context = {
 		'form': form,
-		'events': events
+		'events': events,
+		'advert': advert,
+		'topstory': topstory
 	}
 	return render(request, './home/events.html', context)
-
-
-def about_page(request, *args, **kwargs):
-	form = Form(auto_id=False)
-	if request.method == 'POST':
-		form = Form(request.POST)
-		if form.is_valid():
-			form.clean()
-			form.save()
-			messages.success(request, "Your email was received successfully!", fail_silently = True)
-	else:
-		form = Form()
-	context = {
-		'form': form
-	}
-	return render(request, './home/about.html', context)
 
 
 def story_detail(request, slug, *args, **kwargs):
@@ -76,6 +71,62 @@ def story_detail(request, slug, *args, **kwargs):
 		'story': story
 	}
 	return render(request, './home/story.html', context)
+
+
+def topstories(request, slug, *args, **kwargs):
+	topstory = Topstory.objects.get(slug=slug)
+	topstory_images = Topstory_image.objects.filter(story=topstory)
+	form = Form(auto_id=False)
+	if request.method == 'POST':
+		form = Form(request.POST)
+		if form.is_valid():
+			form.clean()
+			form.save()
+			messages.success(request, "Your email was received successfully!", fail_silently = True)
+	else:
+		form = Form()
+	context = {
+		'form': form,
+		'topstory': topstory,
+		'topstory_images': topstory_images
+	}
+	return render(request, './home/topstories.html', context)
+
+
+def advert(request, slug, *args, **kwargs):
+	advert = Advert.objects.get(slug=slug)
+	advert_images = Advert_image.objects.filter(advert=advert)
+	form = Form(auto_id=False)
+	if request.method == 'POST':
+		form = Form(request.POST)
+		if form.is_valid():
+			form.clean()
+			form.save()
+			messages.success(request, "Your email was received successfully!", fail_silently = True)
+	else:
+		form = Form()
+	context = {
+		'form': form,
+		'advert': advert,
+		'advert_images': advert_images
+	}
+	return render(request, './home/advert.html', context)
+
+
+def about_page(request, *args, **kwargs):
+	form = Form(auto_id=False)
+	if request.method == 'POST':
+		form = Form(request.POST)
+		if form.is_valid():
+			form.clean()
+			form.save()
+			messages.success(request, "Your email was received successfully!", fail_silently = True)
+	else:
+		form = Form()
+	context = {
+		'form': form
+	}
+	return render(request, './home/about.html', context)
 
 
 def profile_feed(request, *args, **kwargs):
